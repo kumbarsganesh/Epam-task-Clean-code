820252cc4d220839ed16fc1fab96e5e2bcf4c256
17211a05e5 <48667165+17211a05e5@users.noreply.github.com>
2020-07-24 16:05:49 +0530

    Add files via upload

---
+++ Cleancode.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+public class Cleancode{
+                public static double Simple(double p,double t,double r){
+		return(p*t*r)/100;
+	}
+              public static double Compound(double p,double t,double r){
+		return p*Math.pow(1.0+r/100.0,t)-p;
+                     }
+              public static void main(String[] args){
+		Simple(2000,2,2);
+		Compound(3000,3,3);
+}
+}
